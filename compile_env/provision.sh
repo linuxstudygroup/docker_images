11aaad9a5e82221f17f3d2a405670568c36d9c01
qniu <qniu@splunk.com>
2019-05-05 17:37:27 +0800

    compile tools

---
+++ compile_env/provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y apt-utils
+apt-get install -y gcc gdb
+apt-get install -y make 
+apt-get install -y vim 
+apt-get install -y git zsh wget
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+mkdir /build 
+
+echo "set nu
+syntax on
+autocmd InsertLeave * se nocul
+autocmd InsertEnter * se cul
+set confirm
+set autoindent
+set cindent
+set expandtab
+set tabstop=4
+set shiftwidth=4
+set mouse=a
+autocmd FileType make set noexpandtab" >> ~/.vimrc
+
